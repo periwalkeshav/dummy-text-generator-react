8128084db72d270294b8f3c8faca78f105ea6904
periwalkeshav <keshav.periwal2018@vitstudent.ac.in>
2021-07-03 00:03:16 +0530

    Text Generator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import data from './data'
 
 function App() {
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState([])
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    let number = parseInt(count)
+    if (count <= 0) {
+      number = 1
+    }
+    if (count > 13) {
+      number = 13
+    }
+    setText(data.slice(0, number))
+  }
+  const handleChange = (e) => {
+    if (e.target.value < 0) {
+      // alert('Value cannot be negative')
+      e.target.value = 0
+    }
+    setCount(e.target.value)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section className='section-center'>
+      <h3>Dummy Text Generator</h3>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='number'>Paragraphs: </label>
+        <input
+          type='number'
+          name='amount'
+          id='number'
+          value={count}
+          onChange={handleChange}
+        />
+        <button type='submit' className='btn'>
+          Generate
+        </button>
+      </form>
+      <article className='lorem-text'>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>
+        })}
+      </article>
+      <footer>
+        <p className='text-bottom'>* Currently there are only 13 paragraphs stored.</p>
+      </footer>
+    </section>
+  )
 }
 
-export default App;
+export default App
